033ded6f3e2a077ac9434ac73886e26fdcf5d588
Hector-Lozada <eeaae7570@gmail.com>
2025-03-20 22:00:29 -0500

    clon

---
+++ app/(routes)/(auth)/components/TitleLogin/TitleLogin.tsx
@@ -0,0 +1,6 @@
+
+export  function TitleLogin() {
+  return (
+    <div>TitleLogin</div>
+  )
+}
---
+++ app/(routes)/(auth)/layout.tsx
@@ -0,0 +1,23 @@
+import { ThemeProvider } from "@/components/theme-provider";
+
+export default function AuthLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        <h1>AuthLayout</h1>
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+      </body>
+    </html>
+  );
+}
