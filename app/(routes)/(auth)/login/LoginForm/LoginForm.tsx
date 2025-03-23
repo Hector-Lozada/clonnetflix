e471a186584eb137fa7315540d44677b9bb825d3
Hector-Lozada <eeaae7570@gmail.com>
2025-03-23 10:42:20 -0500

    update

--- app/(routes)/(auth)/login/LoginForm/LoginForm.tsx
+++ app/(routes)/(auth)/login/LoginForm/LoginForm.tsx
@@ -1,8 +1,66 @@
-import React from "react";
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { formSchema } from "./LoginForm.form";
+import { useState } from "react";
+import { FormError } from "./FormError";
+
+
 export function LoginForm() {
+    const [error, setError] = useState<string | undefined>("");
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            email: "",
+            password: "",
+        },
+    });
+    const onSubmit = (values: z.infer<typeof formSchema>) => {
+        console.log(values);
+    };
     return (
-    <div>
-      <p>Form Login</p>
-    </div>
-    );
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="w-full gap-4 flex flex-col">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input placeholder="Correo Electrónico" {...field} className="h-14 text-white" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input placeholder="Contraseña" type="password" {...field} className="h-14 text-white" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormError message={error}/>
+            <Button type="submit" className="w-full">Iniciar Sesión</Button>
+          </form>
+        </Form>
+      )
 }
